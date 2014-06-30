20b292b4e47913908d2af12c03ef632a3cd6af9b
Bo <patrick.boggan.kane@gmail.com>
2014-06-30 16:02:05 -0400

    completed intro to collections through rectangle data structure manipulation

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,24 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [two-x (+ x x)]
+  (Math/pow two-x two-x))
+  )
 
 (defn spiff [v]
-  :-)
+
+  (if (< (count v) 3) 0 (+ (get v 0) (get v 2)) )
+
+  )
 
 (defn cutify [v]
-  :-)
+    (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[x y z] v]
+     (+ x z)
+  )
+)
 
 (defn point [x y]
   [x y])
@@ -19,22 +27,37 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+(let [[[x1 y1] [x2 y2]] rectangle ]
+  (Math/abs (- x1 x2))
+  )
+)
 
 (defn height [rectangle]
-  :-)
+(let [[[x1 y1] [x2 y2]] rectangle ]
+  (Math/abs (- y1 y2))
+  )
+)
 
 (defn square? [rectangle]
-  :-)
+(let [[[x1 y1] [x2 y2]] rectangle ]
+  (if (= (Math/abs (- x1 x2)) (Math/abs (- y1 y2))) true false)
+)
+  )
 
 (defn area [rectangle]
-  :-)
+(let [[[x1 y1] [x2 y2]] rectangle ]
+   (* (Math/abs (- x1 x2)) (Math/abs (- y1 y2))) )
+)
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle ]
+    (if (and (<= x1 (get point 0) x2) (<= y1 (get point 1) y2)) true false)
+    )
+  )
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (if (and (contains-point? outer (get inner 0)) (contains-point? outer (get inner 1))) true false)
+)
 
 (defn title-length [book]
   :-)
